d49230c157f5fdfe76f35676a42165b913f59241
NastasiakSS <91372675+NastasiakSS@users.noreply.github.com>
2021-11-17 19:54:46 +0200

    Add files via upload

---
+++ Js Homework 1/main.js
@@ -0,0 +1,57 @@
+/*let a = `hello`
+let b = `owu`
+let c = `com`
+let d = `ua`
+let e = 1
+let f = 10
+let g = -999
+let h = 123
+let i = 3.14
+let j = 2.7
+let k = 16
+let l = `true`
+let m = `false`
+
+
+
+console.log(a,b,f,h,m)
+alert(c)
+alert(j)
+alert(k)
+document.write(d, e, g, i, l)*/
+
+
+let firstName = 'Святослав'
+let middleName = 'Русланович'
+let lastName = 'Настосяк'
+let age = 16
+
+
+/*
+prompt('Вітаю!', firstName+middleName)
+
+
+document.write('Тобі', age , 'pоків')
+*/
+
+let a = 100
+let b = 100
+let c = true
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+console.log(5<6)
+console.log(5>6)
+console.log(5==6)
+console.log(5===6)
+console.log(10==10)
+console.log(10===10)
+console.log(10>10)
+console.log(10<10)
+console.log(10!=10)
+console.log(123!==123)
+console.log(123===123)
+
+
