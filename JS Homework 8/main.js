f039ad3e07e4aa56d5b11e31279d7c420d73a1bc
NastasiakSS <91372675+NastasiakSS@users.noreply.github.com>
2021-12-17 19:17:35 +0200

    Add files via upload

---
+++ JS Homework 8/main.js
@@ -0,0 +1,75 @@
+// -- отримує текст з параграфа з id "content"
+
+
+// let contentElement = document.getElementById(`content`)
+// console.log(contentElement);
+
+
+//===============================================================================
+
+
+// -- отримує текст з блоку з id "rules"
+
+// let rulesElement = document.getElementById(`rules`)
+// console.log(rulesElement);
+
+
+//===============================================================================
+
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+
+
+// let contentElement = document.getElementById(`content`)
+// contentElement.innerText= `hello`;
+
+
+// ===============================================================================
+
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+// let rulesElement = document.getElementById(`rules`)
+// rulesElement.innerHTML = `<strong>Whats Uppp</strong>`
+
+
+// ===============================================================================
+
+
+// -- змініть кожному елементу колір фону на червоний
+
+// let elementColor = document.body.children
+// for (const color of elementColor){
+//     color.style.background = `red`
+// }
+
+
+// ===============================================================================
+
+
+// -- змініть кожному елементу колір тексту на синій
+
+// let textColor = document.body.children
+// for (const textColorElement of textColor) {
+//     textColorElement.style.color = `blue`
+// }
+
+
+// ===============================================================================
+
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// let rulesList = document.getElementById(`rules`)
+// console.log(rulesList.classList);
+
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let colorRules = document.getElementsByClassName(`fc_rules`)
+// for (const colorRule of colorRules) {
+//     colorRule.style.color = `red`;
+
+
+
+
